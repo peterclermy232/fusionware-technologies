8dd8b6b29a668d8030adae614d33f62cfed784e9
Peter Atito <peterclermy232@gmail.com>
2025-09-08 14:06:29 +0300

    update the changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
-
-// import your feature components
-
-//import { NavigationComponent } from './sections/navigation/navigation.component';
 import { HeroComponent } from './sections/hero/hero.component';
-//import { ServicesComponent } from './sections/services/services.component';
 import { PortfolioComponent } from './sections/portfolio/portfolio.component';
 import { AboutComponent } from './sections/about/about.component';
-//import { ContactComponent } from './sections/contact/contact.component';
-//import { FooterComponent } from './shared/footer/footer.component';
 import { NavigationComponent } from './shared/navigation/navigation.component';
 import { ServicesComponent } from './sections/services/services.component';
 import { ContactComponent } from './sections/contact/contact.component';
